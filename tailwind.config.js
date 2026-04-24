554273e54c68ee072e3c3cf422296724e37382f9
HarisNaveed17 <haris.naveed174@gmail.com>
2026-04-24 16:23:38 +0500

    update profile picture

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,7 +23,7 @@ module.exports = {
         'trebuchet-pixel': ['Trebuchet MS pixelated', 'sans-serif'],
       },
       backgroundImage: ({
-        'profile-pic': "url('/img/profile-picture.webp')",
+        'profile-pic': "url('/img/pfp.webp')",
         'office-pic': "url('/img/office-picture.webp')",
         'window-picture': "url('/img/icons/pictures/bg-picture.webp')",
         'window-white': 'linear-gradient(to right, rgb(237, 237, 229) 0%, rgb(237, 232, 205) 100%)',
@@ -91,6 +91,7 @@ module.exports = {
         'px': '1px',
         '9/12': '75%',
         '3/12': '25%',
+        'content-window': '100%',
       },
       zIndex: {
         'max': '999999',
