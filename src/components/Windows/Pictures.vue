11f6020f279dc6312923879dc4262aa597152e52
HarisNaveed17 <haris.naveed174@gmail.com>
2026-06-10 01:37:34 +0500

    add photos

--- src/components/Windows/Pictures.vue
+++ src/components/Windows/Pictures.vue
@@ -2,20 +2,27 @@
   <div class="relative right-0 h-full flex overflow-hidden">
     <WindowLeftMenu :leftMenuType="props.leftMenuType" />
     <!-- Content of project -->
-    <div class="w-full h-full bg-pictures-blue overflow-x-hidden">
+    <div class="w-full h-full bg-pictures-blue overflow-x-hidden flex flex-col">
       <!-- Carroussel -->
-      <div class="flex flex-col justify-center items-center w-full h-9/12 gap-1">
-        <div v-if="currentPicture" class="w-3/4 h-5/6 mt-1 border border-black overflow-hidden">
-          <div
-            class="w-full h-full bg-contain bg-center bg-no-repeat"
-            :style="{
-              backgroundImage: `url(${currentPicture.url})`,
-              transform: `rotate(${rotation}deg)`
-            }"
-          ></div>
+      <div class="flex flex-col items-center w-full flex-1 min-h-0 overflow-hidden">
+        <div v-if="currentPicture" class="flex flex-col items-center flex-1 min-h-0 w-full max-w-[90%] pt-2">
+          <div class="flex-1 min-h-0 flex items-center justify-center w-full">
+            <img
+              :src="currentPicture.src"
+              :alt="currentPicture.alt"
+              class="max-w-full max-h-full w-auto h-auto border border-black"
+              :style="{ transform: `rotate(${rotation}deg)` }"
+            />
+          </div>
+          <p class="font-trebuchet-pixel text-base text-black italic text-center mt-1 px-2 shrink-0">
+            {{ currentPicture.description }}
+          </p>
+          <p class="font-trebuchet-pixel text-sm text-black text-center px-2 shrink-0">
+            {{ currentPicture.location }}
+          </p>
         </div>
         <p v-else>Loading...</p>
-        <div class="flex py-2">
+        <div class="flex py-2 shrink-0">
           <div class="flex gap-0.5">
             <button
               @click="previousPicture"
@@ -48,17 +55,18 @@
         </div>
       </div>
       <!-- Footer preview image -->
-      <div class="w-full h-3/12 bg-white">
-        <div class="flex w-full h-full bg-no-repeat bg-32 bg-bottom-right-picture-menu bg-window-picture px-2 pt-1.5 pb-5 gap-4 overflow-x-auto">
-          <div ref="pictureContainer" v-for="(picture, index) in pictures" :key="picture.id" class="w-full h-full flex flex-col items-center">
-            <div
+      <div class="w-full bg-white">
+        <div class="flex w-full bg-no-repeat bg-32 bg-bottom-right-picture-menu bg-window-picture px-2 pt-1.5 pb-2 gap-4 overflow-x-auto">
+          <div ref="pictureContainer" v-for="(picture, index) in pictures" :key="picture.id" class="flex-shrink-0 flex flex-col items-center">
+            <img
               ref="pictureElements"
               @click="setCurrentPicture(picture)"
-              :class="
-                'w-full h-full min-w-20 bg-no-repeat bg-contain bg-center cursor-pointer' +
-                (currentPicture && currentPicture.id === picture.id ? ' border-3 border-focus-blue' : ' border border-gray-300')
-              "
-              :style="{ backgroundImage: 'url(' + picture.url + ')' }"
+              :src="picture.src"
+              :alt="picture.alt"
+              :class="[
+                'h-16 w-auto cursor-pointer object-contain',
+                currentPicture && currentPicture.id === picture.id ? 'border-3 border-focus-blue' : 'border border-gray-300'
+              ]"
             />
             <p
               @click="setCurrentPicture(picture)"
@@ -98,8 +106,6 @@ onMounted(() => {
       currentPicture.value = pictures[0]
       nextTick(() => scrollToCurrentPicture())
     }, 100)
-  } else {
-    console.error('No pictures available in picturesData')
   }
 })
 
